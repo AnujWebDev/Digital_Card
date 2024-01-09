29bf98addc511c4968609bdd6c49d7c44fd1bdfb
unknown <anujyash10gmail.com>
2024-01-09 14:26:40 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+// Vite configuration
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['react-fast-Marquee'],
+    },
+  },
+});
